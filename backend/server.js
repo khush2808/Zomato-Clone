363ff480525f438f0bd7707453e9ccd4d4991c58
Khush-Shah <khushshah675@gmail.com>
2024-09-04 16:28:42 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 //middleware
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 //db connection
 connectDB();
