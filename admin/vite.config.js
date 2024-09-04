3658aa7c19aa68ba5519f8b7dd2b99ce8b8af251
Khush-Shah <khushshah675@gmail.com>
2024-09-04 16:37:41 +0530

    fixing cors error

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+	server: {
+    proxy: {
+    '/': {
+      target: 'https://zomato-clone-three-rho.vercel.app/',
+      changeOrigin: true,
+      secure: false,
+      rewrite: (path) => path.replace(/^\//, '')
+    },
+    cors:false
+    },
+  },
+  define: {
+    'process.env': {}
+  }
+}
+)
